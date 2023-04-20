72df46b6edc6085b6af2ed1fcb3af0f6e5e38398
Minji Kang <sohotmj@naver.com>
2023-04-20 22:17:31 +0900

    0420 react study

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+/* eslint-disable */
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  let [title,setTitle] = useState(['남자코트 추천','강남우동 맛집','영화제 즐기기']);
+  let [like, setLike] = useState(0);
+  let posts = '강남고기 맛집';
+  
+  function titleupd(){
+    var newtitle = [...title];
+    var txt='';
+    if(newtitle[0].indexOf('남자')>-1){
+      txt='여자';
+    }else{
+      txt='남자';
+    }
+    newtitle[0] = txt+"코트추천";
+    setTitle(newtitle);
+  }
+
+  function titlearray(){
+    var titlearr = [...title];
+    titlearr.sort();
+    setTitle(titlearr);
+  }
+
+  function likeup(){
+    setLike(like+1);
+  }
+
   return (
+ 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='black-nav'>
+      <div>개발 블로그</div>
+     </div>
+
+    <button onClick={titleupd}>button</button>
+
+     <div className='list'>
+      <h3 id='title1'>{title[0]}<span onClick={likeup}>❤️</span>{like}</h3>
+      <p>4월20일 발행</p>
+      <hr/>
+    </div>
+
+    <div className='list'>
+      <h3>{title[1]}</h3>
+      <p>4월24일 발행</p>
+      <hr/>
+    </div>
+    <div className='list'>
+      <h3>{title[2]}</h3>
+      <p>4월29일 발행</p>
+      <hr/>
+    </div>
+   <Modal/>
+
     </div>
   );
+    function Modal(){
+      return(
+        <div className='modal'>
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+      )
+    }
+  
 }
 
 export default App;
