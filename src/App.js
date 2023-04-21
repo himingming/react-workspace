a6deaf1aea7443df04993cbc49f276f99d9f7afc
Minji Kang <sohotmj@naver.com>
2023-04-21 21:52:20 +0900

    0421 props공부

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import React, {useState} from 'react';
 function App() {
 
   let [title,setTitle] = useState(['남자코트 추천','강남우동 맛집','영화제 즐기기']);
-  let [like, setLike] = useState(0);
+  let [like, setLike] = useState([0,0,0]);
+  let [arrtitle, setArrtitle] = useState(0);
+
   let posts = '강남고기 맛집';
+  let [modal, setModal] = useState(false);
   
   function titleupd(){
     var newtitle = [...title];
@@ -30,6 +33,7 @@ function App() {
     setLike(like+1);
   }
 
+  
   return (
  
     <div className="App">
@@ -39,7 +43,7 @@ function App() {
 
     <button onClick={titleupd}>button</button>
 
-     <div className='list'>
+     {/* <div className='list'>
       <h3 id='title1'>{title[0]}<span onClick={likeup}>❤️</span>{like}</h3>
       <p>4월20일 발행</p>
       <hr/>
@@ -49,22 +53,36 @@ function App() {
       <h3>{title[1]}</h3>
       <p>4월24일 발행</p>
       <hr/>
-    </div>
-    <div className='list'>
-      <h3>{title[2]}</h3>
-      <p>4월29일 발행</p>
-      <hr/>
-    </div>
-   <Modal/>
-
+    </div> */}
+      
+      {
+        title.map(function(a, i){
+          return (
+          <div className='list'>
+          <h3 onClick={() => {
+            setModal(!modal)
+          }}>{title[i]}<span onClick={()=>{
+            let likey = [...like];
+            likey[i] = likey[i]+1;
+            setLike(likey)
+          }}>❤️</span>{like[i]}</h3>
+          <p>4월29일 발행</p>
+          <hr/>
+        </div>)
+        })
+      }
+ {
+      modal == true ? <Modal title={title} titleupd={titleupd} arrtitle={arrtitle}/> : null
+      }
     </div>
   );
-    function Modal(){
+    function Modal(props){
       return(
         <div className='modal'>
-        <h2>제목</h2>
+        <h2>{props.title[props.arrtitle]}</h2>
         <p>날짜</p>
         <p>상세내용</p>
+        <button onClick={props.titleupd}>update</button>
       </div>
       )
     }
